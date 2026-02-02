0e1d0bbb590ecebf8897b3d7a208ba3cfc5464d4
Rifal Abdul Ghozali <rifalghozali8@gmail.com>
2026-02-02 23:00:25 +0700

    fix api update & delete

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,7 @@ use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('kelas', KelasController::class);
+Route::apiResource('kelas', KelasController::class)->parameters([
+    'kelas' => 'kelas'
+]);;
 Route::apiResource('users', UserController::class);
